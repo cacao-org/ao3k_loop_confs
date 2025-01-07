c1d89a1952198ae74568fe7a4bca1468be330b97
kyohoon-ahn <dksrygns@gmail.com>
2025-01-07 09:37:31 +0000

    Initial commit of AO3k conf folders

---
+++ backport_to_cacao_repo.bash
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+cd ~/AOloop/
+
+cp ./apd188-rootdir/cacaovars.apd188.bash ${MILK_ROOT}/plugins/cacao-src/AOloopControl/examples/ao3k-apd188-conf/cacaovars.bash
+cp ./bimdm3kpt-rootdir/cacaovars.bimdm3kpt.bash ${MILK_ROOT}/plugins/cacao-src/AOloopControl/examples/ao3k-bimdm3kpt-conf/cacaovars.bash
+cp ./lowfs3k-rootdir/cacaovars.lowfs3k.bash ${MILK_ROOT}/plugins/cacao-src/AOloopControl/examples/ao3k-lowfs3k-conf/cacaovars.bash
+cp ./nir3k-rootdir/cacaovars.nir3k.bash ${MILK_ROOT}/plugins/cacao-src/AOloopControl/examples/ao3k-nirpyr3k-conf/cacaovars.bash
+cp ./ttoff188-rootdir/cacaovars.ttoff188.bash ${MILK_ROOT}/plugins/cacao-src/AOloopControl/examples/ao3k-ttoff188-conf/cacaovars.bash
+cp ./apd3k-rootdir/cacaovars.apd3k.bash ${MILK_ROOT}/plugins/cacao-src/AOloopControl/examples/ao3k-apd3k-conf/cacaovars.bash
+cp ./lowfs188-rootdir/cacaovars.lowfs188.bash ${MILK_ROOT}/plugins/cacao-src/AOloopControl/examples/ao3k-lowfs188-conf/cacaovars.bash
+cp ./nir188-rootdir/cacaovars.nir188.bash ${MILK_ROOT}/plugins/cacao-src/AOloopControl/examples/ao3k-nirpyr188-conf/cacaovars.bash
+cp ./nlcwfs188-rootdir/cacaovars.nlcwfs188.bash ${MILK_ROOT}/plugins/cacao-src/AOloopControl/examples/ao3k-nlcwfs188-conf/cacaovars.bash
+cp ./ttoff3k-rootdir/cacaovars.ttoff3k.bash ${MILK_ROOT}/plugins/cacao-src/AOloopControl/examples/ao3k-ttoff3k-conf/cacaovars.bash
+
+cp ./apd188-rootdir/fpssetup.setval.apd188.conf ${MILK_ROOT}/plugins/cacao-src/AOloopControl/examples/ao3k-apd188-conf/fpssetup.setval.conf
+cp ./bimdm3kpt-rootdir/fpssetup.setval.bimdm3kpt.conf ${MILK_ROOT}/plugins/cacao-src/AOloopControl/examples/ao3k-bimdm3kpt-conf/fpssetup.setval.conf
+cp ./lowfs3k-rootdir/fpssetup.setval.lowfs3k.conf ${MILK_ROOT}/plugins/cacao-src/AOloopControl/examples/ao3k-lowfs3k-conf/fpssetup.setval.conf
+cp ./nir3k-rootdir/fpssetup.setval.nir3k.conf ${MILK_ROOT}/plugins/cacao-src/AOloopControl/examples/ao3k-nirpyr3k-conf/fpssetup.setval.conf
+cp ./ttoff188-rootdir/fpssetup.setval.ttoff188.conf ${MILK_ROOT}/plugins/cacao-src/AOloopControl/examples/ao3k-ttoff188-conf/fpssetup.setval.conf
+cp ./apd3k-rootdir/fpssetup.setval.apd3k.conf ${MILK_ROOT}/plugins/cacao-src/AOloopControl/examples/ao3k-apd3k-conf/fpssetup.setval.conf
+cp ./lowfs188-rootdir/fpssetup.setval.lowfs188.conf ${MILK_ROOT}/plugins/cacao-src/AOloopControl/examples/ao3k-lowfs188-conf/fpssetup.setval.conf
+cp ./nir188-rootdir/fpssetup.setval.nir188.conf ${MILK_ROOT}/plugins/cacao-src/AOloopControl/examples/ao3k-nirpyr188-conf/fpssetup.setval.conf
+cp ./nlcwfs188-rootdir/fpssetup.setval.nlcwfs188.conf ${MILK_ROOT}/plugins/cacao-src/AOloopControl/examples/ao3k-nlcwfs188-conf/fpssetup.setval.conf
+cp ./ttoff3k-rootdir/fpssetup.setval.ttoff3k.conf ${MILK_ROOT}/plugins/cacao-src/AOloopControl/examples/ao3k-ttoff3k-conf/fpssetup.setval.conf
